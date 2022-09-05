e9b8ba5384e3035fc21173a2661c4455be987bfa
joemakescool <joebarker@mail.weber.edu>
2022-09-05 15:31:11 -0600

    made a server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// Module Dependencies
+const express = require('express')
+// work please. what doesn't the node-sass load on heroku
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use(serveStatic(path.join(__dirname, 'dist')))
+// app.use(serveStatic(path.join(__dirname, 'sass')))
+const port = process.env.PORT || 80
+app.listen(port)
